739247c6cf0e7e0efd5e4bfd3b14940859e445ac
imran414141 <imrank2018@namal.edu.pk>
2020-09-18 19:27:31 +0500

    final

---
+++ main.c
@@ -0,0 +1,222 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int x=1;
+while(x<=5)
+{
+int y=14;
+while(y>=x)
+{
+printf(" ");
+y--;
+}
+int z=2;
+while(z<=x)
+{
+printf("*");
+z++;
+}
+int u=1;
+while(u<=x)
+{
+printf("*");
+u++;
+}
+x++;
+printf("\n");
+}
+
+
+//2nd part of star;
+
+
+
+
+
+int xx=1;
+while(xx<=5)
+{
+int ab=2;
+while(ab<=xx)
+{
+printf(" ");
+ab++;
+}
+int yy=4;
+while(yy>=xx)
+{
+printf("*");
+yy--;
+}
+int zz=5;
+while(zz>=xx)
+{
+printf("*");
+zz--;
+}
+int ab1=1;
+while(ab1<=xx)
+{
+printf(" ");
+ab1++;
+}
+printf("         ");
+int ab2=1;
+while(ab2<=xx)
+{
+printf(" ");
+ab2++;
+}
+int ab3=5;
+while(ab3>=xx)
+{
+printf("*");
+ab3--;
+}
+int ab4 =4;
+while(ab4>=xx)
+{
+printf("*");
+ab4--;
+}
+xx++;
+printf("\n");
+}
+
+printf("    ");
+
+//thrd part;
+
+
+int th1=1;
+while(th1<=3)
+{
+printf("*");
+th1++;
+}
+
+int th2=1;
+while(th2<=15)
+{
+printf(" ");
+th2++;
+}
+int th3=1;
+while(th3<=3)
+{
+printf("*");
+th3++;
+}
+printf("\n");
+printf("    ");
+
+
+
+
+int th4=1;
+while(th4<=6)
+{
+printf("*");
+th4++;
+}
+int th5=1;
+while(th5<=9)
+{
+printf(" ");
+th5++;
+}
+
+
+int lst1=1;
+while(lst1<=6)
+{
+printf("*");
+lst1++;
+}
+printf("\n");
+printf("    ");
+int lst2=1;
+while(lst2<=21)
+{
+printf("*");
+lst2++;
+}
+printf("\n");
+printf("    ");
+int lst11=1;
+while(lst11<=6)
+{
+printf("*");
+lst11++;
+}
+printf("         ");
+
+//-------------
+int lst22=1;
+while(lst22<=6)
+{
+printf("*");
+lst22++;
+}
+printf("\n");
+printf("    ");
+int lst3=1;
+while(lst3<=4)
+{
+printf("*");
+lst3++;
+}
+printf("             ");
+int lst4=1;
+while(lst4<=4)
+{
+printf("*");
+lst4++;
+}
+printf("\n");
+printf("    ");
+
+
+//------------
+
+
+int lst33=1;
+while(lst33<=2)
+{
+printf("*");
+lst33++;
+}
+
+int lst44=1;
+while(lst44<=17)
+{
+printf(" ");
+lst44++;
+}
+
+
+int lst5=1;
+while(lst5<=2)
+{
+printf("*");
+lst5++;
+}
+printf("\n");
+printf("    ");
+printf("*");
+
+
+
+int lst55=1;
+while(lst55<=19)
+{
+printf(" ");
+lst55++;
+}
+printf("*");
+printf("\n");
+
+    return 0;
+}
